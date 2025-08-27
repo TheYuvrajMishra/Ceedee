d554d8538b2521f687bbe6abffe4515e8702dd2e
TheYuvrajMishra <yuvraj17mishra11@gmail.com>
2025-08-27 14:20:21 +0530

    .

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter,Routes, Route } from "react-router"
 import HeroSection from "./Pages/Main/HeroSection"
-
+import Navbar from "./Components/Navbar"
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HeroSection/>}></Route>
       </Routes>
